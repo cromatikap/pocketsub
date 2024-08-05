dc072e3ac8a250be7916ce03cfffb1d7e3a6485f
bidetaggle <bidetaggle@gmail.com>
2024-08-05 23:41:56 +0100

    simplify css

--- src/app/[shopAddress]/page.tsx
+++ src/app/[shopAddress]/page.tsx
@@ -1,9 +1,13 @@
 
+import PageTitle from '@/components/PageTitle';
 import UserInfo from '@/components/UserInfo';
 
 export default function Page() {
 
   return <>
-    <UserInfo />
-  </>;
+    <div className="flex justify-between items-start p-2">
+      <PageTitle params={{ title: "Shop", walletAddress: "0x612d...ddc5" }} />
+      <UserInfo />
+    </div>
+  </>
 }
