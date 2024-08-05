300dc8f8e1300c5a04f4be2270527e1376e49bfe
bidetaggle <bidetaggle@gmail.com>
2024-08-05 13:54:28 +0100

    add flowbite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Alert, Button } from "flowbite-react";
+
 export default function Home() {
   return <>
     Home page
+    <Alert color="info">Alert!</Alert>
+    <Button>Button</Button>
   </>;
 }
