3e5373b0bb478f3fc64483629b8b4909296e6fb6
xdmaury <maury1418@hotmail.com>
2024-08-05 11:43:26 -0300

    Adjust card params

--- src/app/[shopAddress]/page.tsx
+++ src/app/[shopAddress]/page.tsx
@@ -1,5 +1,17 @@
-export default function Page({ params }: { params: { shopAddress: string } }) {
-  return <>
-    Shop page, wallet address: {params.shopAddress}
-  </>
+import { Card } from "flowbite-react";
+
+export default function Page({ params }: { params: { name: string, address: string, description: string } }) {
+  return (
+    <Card href="#">
+      <h1 className="font-bold tracking-tight text-gray-900 dark:text-white truncate">
+        {params.name}
+      </h1>
+      <h2 className="tracking-tight text-gray-900 dark:text-white truncate">
+        {params.address}
+      </h2>
+      <p className="text-sm text-gray-700 dark:text-gray-400 ">
+        {params.description}
+      </p>
+    </Card>
+  );
 }
