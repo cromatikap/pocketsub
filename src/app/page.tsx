bda7378509c9a47fab478b77a41dde2737775867
bidetaggle <bidetaggle@gmail.com>
2024-08-05 18:39:54 +0100

    move shop card into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,25 @@
-import { Button, Card } from "flowbite-react";
+import ShopCard from "@/components/ShopCard";
+import { Button } from "flowbite-react";
 
+const ShopsList = [
+  {
+    name: "Ipal base of the world",
+    address: "0xa",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed quam a dui tempor fringilla sed sed diam."
+  },
+  {
+    name: "Ipal base of the world 1",
+    address: "0xb",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed quam a dui tempor fringilla sed sed diam."
+  },
+  {
+    name: "Ipal base of the world some large title here for test 2",
+    address: "0xc",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed quam a dui tempor fringilla sed sed diam."
+  }
+];
 
 export default function Home() {
-  const ShopsList = [
-    {
-      name: "Ipal base of the world",
-      address: "0xabcd...000",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed quam a dui tempor fringilla sed sed diam."
-    },
-    {
-      name: "Ipal base of the world 1",
-      address: "0xabcd...001",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed quam a dui tempor fringilla sed sed diam."
-    },
-    {
-      name: "Ipal base of the world some large title here for test 2",
-      address: "0xabcd...000000000000000000000000000000002",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed quam a dui tempor fringilla sed sed diam."
-    }
-  ];
   return (
     <>
       <div className="mb-4 flex flex-col gap-4 text-center">
@@ -32,19 +33,9 @@ export default function Home() {
       <div className="mb-4">
         <h2>Browser our community</h2>
       </div>
-      <ul className="space-y-1 w-full overflow-y-auto rounded-lg">
+      <ul>
         {ShopsList.map((shop, index) => (
-          <Card href="#" key={index}>
-            <h1 className="font-bold tracking-tight text-gray-900 dark:text-white truncate">
-              {shop.name}
-            </h1>
-            <h2 className="tracking-tight text-gray-900 dark:text-white truncate">
-              {shop.address}
-            </h2>
-            <p className="text-sm text-gray-700 dark:text-gray-400 ">
-              {shop.description}
-            </p>
-          </Card>
+          <ShopCard params={shop} key={index} />
         ))}
       </ul>
 
