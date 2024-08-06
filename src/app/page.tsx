1df4a6abbf44f64d9ade0a56dcceeabd06f4db76
bidetaggle <bidetaggle@gmail.com>
2024-08-06 21:16:22 +0100

    add the login flow for create your shop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import ShopCard from "@/components/ShopCard";
+import { useWeb3Auth } from "@/components/Web3AuthProvider";
 import { Button, HR } from "flowbite-react";
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Spinner } from "flowbite-react";
 
 const ShopsList = [
   {
@@ -23,15 +25,39 @@ const ShopsList = [
 ];
 
 const Home = () => {
+  const { loggedIn, getAccounts, login } = useWeb3Auth();
+  const [isLoading, setIsLoading] = useState(true);
+  const [walletAddress, setWalletAddress] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getWalletAddress = async () => {
+      if (loggedIn) {
+        const accounts = await getAccounts();
+        setWalletAddress(accounts[0]);
+      }
+      setIsLoading(false);
+    }
+
+    getWalletAddress();
+  }, [loggedIn]);
+
+  const handleCreateShop = async () => {
+    if (!loggedIn){
+      await login();
+      const accounts = await getAccounts();
+      window.location.href = `/${accounts[0]}`;
+    }
+    else
+      window.location.href = `/${walletAddress}`;
+  }
+
   return (
     <>
       <h1>Pocket Sub</h1>
       <h2>on-chain subscription market</h2>
-      <Link href="/shop">
-        <Button className="m-auto" size="xl" gradientMonochrome="lime">
-          Create your shop
+        <Button onClick={handleCreateShop} className="m-auto" size="xl" gradientMonochrome="lime">
+          {isLoading ? <Spinner /> : <>Create your shop</>}
         </Button>
-      </Link>
       <HR />
       <div className="flex flex-wrap justify-evenly">
         {ShopsList.map((shop, index) => (
