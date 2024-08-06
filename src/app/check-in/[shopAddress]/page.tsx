60783161ce613aba0b544419416966f0b2f7803b
xdmaury <maury1418@hotmail.com>
2024-08-06 14:23:28 -0300

    Insert qrcode component

--- src/app/check-in/[shopAddress]/page.tsx
+++ src/app/check-in/[shopAddress]/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import PageTitle from "@/components/PageTitle";
+import QRScanner from "@/components/QRScanner";
+import { Label, TextInput } from "flowbite-react";
+import { useState } from "react";
 
 const Page = ({ params }: { params: { shopAddress: string } }) => {
-  return(
-    <>Check-in page, shop address: {params.shopAddress}</>
+
+  const [qrscannerResult, setQRScannerResult] = useState<any | null>(null);
+
+  return (
+    <>
+      <PageTitle title="Check-in page" walletAddress={params.shopAddress} />
+      {/* back butto here */}
+      <QRScanner onScan={(result) => setQRScannerResult(result)} />
+
+      <div className="text-center">
+        <div className="mb-2 block">
+          <Label htmlFor="walletAddress" value="WalletAddress" />
+        </div>
+        <TextInput id="walletAddress" type="text" sizing="md" value={JSON.stringify(qrscannerResult)} disabled />
+      </div>
+    </>
   );
 }
 
