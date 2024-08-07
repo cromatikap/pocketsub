8178b653dbd033dc12bb82aaeae7df72f3c1ef21
bidetaggle <bidetaggle@gmail.com>
2024-08-07 11:36:55 +0100

    add shop address to back button href

--- src/app/identity/[shopAddress]/page.tsx
+++ src/app/identity/[shopAddress]/page.tsx
@@ -2,11 +2,14 @@ import BackButton from "@/components/BackButton";
 import QRCode from "react-qr-code";
 
 export default function Page({ params }: { params: { shopAddress: string } }) {
+
+  const userAddress = "0x123";
+
   return (
-    <div className="mx-auto flex w-full flex-col items-center gap-8 my-10">
-      <BackButton href='/shopAddress' name='Back to shop'/>
-      <QRCode value={params.shopAddress} className="max-w-lg h-auto w-full" />
-      <p className="font-bold">Wallet Address {params.shopAddress}</p>
-    </div>
+    <>
+      <BackButton href={`/${params.shopAddress}`} name='Back to shop'/>
+      <QRCode value={userAddress} className="max-w-lg h-auto w-full" />
+      <p className="font-bold">Wallet Address {userAddress}</p>
+    </>
   );
 }
