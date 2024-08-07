a5d7adbbb4f274fe0a68d6dceff43d2f837c465a
bidetaggle <bidetaggle@gmail.com>
2024-08-07 11:43:30 +0100

    add arrow left icon in Back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,7 +1,11 @@
 import { Button } from "flowbite-react";
+import { HiOutlineArrowLeft } from "react-icons/hi";
 
 const BackButton: React.FC<{ name: string; href: string; }> = (props) => {
-    return <Button href={props.href}>{props.name}</Button>
+  return <Button href={props.href}>
+    <HiOutlineArrowLeft className="mr-2 h-5 w-5" />
+    {props.name}
+  </Button>
 }
 
 export default BackButton;
