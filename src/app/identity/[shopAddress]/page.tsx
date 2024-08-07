40536d90f134bed7c680a2dfaf09446effbd1652
bidetaggle <bidetaggle@gmail.com>
2024-08-07 11:45:19 +0100

    add PageTitle component to identity page

--- src/app/identity/[shopAddress]/page.tsx
+++ src/app/identity/[shopAddress]/page.tsx
@@ -1,4 +1,5 @@
 import BackButton from "@/components/BackButton";
+import PageTitle from "@/components/PageTitle";
 import QRCode from "react-qr-code";
 
 export default function Page({ params }: { params: { shopAddress: string } }) {
@@ -7,6 +8,7 @@ export default function Page({ params }: { params: { shopAddress: string } }) {
 
   return (
     <>
+      <PageTitle title="Identity" />
       <BackButton href={`/${params.shopAddress}`} name='Back to shop'/>
       <QRCode value={userAddress} className="max-w-lg h-auto w-full" />
       <p className="font-bold">Wallet Address {userAddress}</p>
