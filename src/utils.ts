1e38c512d080d1c8e0e6c2f0e247f0cf48abed89
bidetaggle <bidetaggle@gmail.com>
2024-08-10 15:22:38 +0100

    update contract

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import web3 from "web3";
 
 const { abi } = contract;
 
-const CONTRACT_ADDRESS = "0x78F120AA24493637fc6Be69C946F98AF1C84A528";
+const CONTRACT_ADDRESS = "0xD180d0697F533D921F28A92Aa1cdE775Bbff4B95";
 
 const shrinkWalletAddress = (walletAddress: string) => {
   const start = walletAddress.slice(0, 6);
