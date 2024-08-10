717d100fcce8be5a63c9f5576a9d24adf01c7799
bidetaggle <bidetaggle@gmail.com>
2024-08-10 16:42:46 +0100

    fix hydration error

--- src/app/identity/[shopAddress]/page.tsx
+++ src/app/identity/[shopAddress]/page.tsx
@@ -4,10 +4,15 @@ import BackButton from "@/components/BackButton";
 import PageTitle from "@/components/PageTitle";
 import QRCode from "react-qr-code";
 import { useAccount } from "wagmi";
+import { useState, useEffect } from "react";
 
 export default function Page({ params }: { params: { shopAddress: string } }) {
-
   const { address } = useAccount();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return (
     <>
@@ -15,7 +20,7 @@ export default function Page({ params }: { params: { shopAddress: string } }) {
         <PageTitle title="Identity" />
         <BackButton href={`/${params.shopAddress}`} name='Back to store'/>
       </div>
-      {address &&
+      {isMounted && address &&
         <div className="flex flex-col gap-8 p-8 m-auto items-center">
           <QRCode value={address} className="max-w-lg h-auto w-full" />
           <div className="text-xs font-mono">{address}</div>
